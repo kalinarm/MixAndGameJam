25254dc4095c6130bcacee50c0c196f0db258982
Kal <kalinarm@gmail.com>
2020-11-13 23:22:40 +0100

    integrate dice model

---
+++ Assets/MixJam/Scripts/DiceZone.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MG
+{
+    public class DiceZone : MonoBehaviour
+    {
+        public enum ZONE_EFFECT
+        {
+            NOTHING,
+            DICE_PLUS,
+            DICE_MINUS,
+            DICE_MULTIPLICATOR
+        }
+
+        [System.Serializable]
+        public class DiceZoneParam
+        {
+            public ZONE_EFFECT effect = ZONE_EFFECT.NOTHING;
+            public int effectParameter = 0;
+        }
+
+        public DiceZoneParam config = new DiceZoneParam();
+    }
+}

408764b9d3657056715980f86b2393f895933d4e
Esben <esbennj@hotmail.com>
2020-11-14 15:52:17 +0100

    Sound Tester: Editor Script

---
+++ Assets/MixJam/Fx/SoundTester.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace MG
+{
+    public class SoundTester : MonoBehaviour
+    {
+        public AudioTrigger audioTrigger;
+
+        public void PlayAudioTrigger()
+        {
+            AudioSource audioSource = GetComponent<AudioSource>();
+            if (audioSource != null)
+                DestroyImmediate(audioSource);
+            Debug.Log("play sound");
+            audioTrigger.trigger(gameObject);
+        }
+    }
+}
---
+++ Assets/MixJam/Fx/SoundTesterEditor.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+namespace MG
+{
+    [CustomEditor(typeof(SoundTester), true)]
+    public class SoundTesterEditor : Editor
+    {
+        public override void OnInspectorGUI()
+        {
+            DrawDefaultInspector();
+
+            EditorGUI.BeginDisabledGroup(serializedObject.isEditingMultipleObjects);
+
+            if (GUILayout.Button("Play Audio Trigger"))
+            {
+                ((SoundTester)target).PlayAudioTrigger();
+            }
+
+            EditorGUI.EndDisabledGroup();
+        }
+    }
+}

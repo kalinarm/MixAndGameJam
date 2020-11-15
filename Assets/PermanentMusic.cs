f963408c2dc341079a01ac44534f9caf41fba7bc
Esben <esbennj@hotmail.com>
2020-11-15 15:05:37 +0100

    music loop inserted in menu scene

---
+++ Assets/PermanentMusic.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PermanentMusic : MonoBehaviour
+{
+    public static bool musicTriggered;
+
+    private void Awake()
+    {
+        if (!musicTriggered)
+        {
+            DontDestroyOnLoad(gameObject);
+            musicTriggered = true;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+}

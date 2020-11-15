edd8c51f7f3011f921a9e1811ffde749935a9dce
Esben <esbennj@hotmail.com>
2020-11-15 16:07:47 +0100

    new music, and better sfx

---
+++ Assets/MusicBehavior.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicBehavior : MonoBehaviour
+{
+    public static bool musicTriggered;
+    AudioSource audioSource;
+    public float fadeInTime;
+
+    private void Awake()
+    {
+        if (!musicTriggered)
+        {
+            audioSource = GetComponent<AudioSource>();
+            DontDestroyOnLoad(gameObject);
+            musicTriggered = true;
+            VolumeFadingMusic volFadeMusic = GetComponent<VolumeFadingMusic>();
+            volFadeMusic.SetInitialVolume(audioSource);
+            volFadeMusic.VolumeFade(audioSource, 1, fadeInTime);
+            audioSource.Play();
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+}
